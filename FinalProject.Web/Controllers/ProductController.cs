b0a9a42ebb0c09f7acf2e9fc76ea08a6e49f6394
ParvizHajili <parviz.hajili@gmail.com>
2024-09-09 20:53:30 +0400

    Add project files.

---
+++ FinalProject.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using FinalProject.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new()
+            {
+                new Product {Id=1,Name="Noutbuk"},
+                new Product {Id=2,Name="Cpu"},
+                new Product {Id=3,Name="Ram"},
+                new Product {Id=4,Name="Kuller fan"},
+            };
+
+            return View(products);
+        }
+    }
+}
